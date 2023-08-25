138f777b5561635fa8f72b94d3698ef66299c74f
Phantomlsh <lishiheng2001@163.com>
2023-08-25 17:44:50 +0800

    Add audio

--- src/routes/word/+page.svelte
+++ src/routes/word/+page.svelte
@@ -55,6 +55,13 @@
   if (!data.user) goto('/')
   else init()
 
+  let audio
+  function play () {
+    if (audio) audio.pause()
+    audio = new Audio(`https://dict.youdao.com/dictvoice?audio=${current.word}&type=1`)
+    audio.play()
+  }
+
   let show = false
   let current = {}
   let currentPro = {}
@@ -64,17 +71,23 @@
     show = false
     // find review
     currentPro = await model.pro.first()
-    if (currentPro && currentPro.due <= model.time()) return current = await model.lib.get(id2_id(currentPro.id))
+    if (currentPro && currentPro.due <= model.time()) {
+      current = await model.lib.get(id2_id(currentPro.id))
+      return play()
+    }
     // new word
     const newID = newIDs.shift()
     newIDs = newIDs
     if (newID) {
       currentPro = { id: newID, step: 0, time: model.time(), due: model.time() }
       current = await model.lib.get(id2_id(newID))
-      return
+      return play()
     }
     // find review loosely
-    if (currentPro && currentPro.due <= model.time() + 25) return current = await model.lib.get(id2_id(currentPro.id))
+    if (currentPro && currentPro.due <= model.time() + 25) {
+      current = await model.lib.get(id2_id(currentPro.id))
+      return play()
+    }
     complete()
   }
 
@@ -104,18 +117,22 @@
 <div class="h-screen w-screen px-4 py-16 bg-gray-100 flex flex-col items-center justify-between">
   {#if current.word}
     <div class="flex flex-col items-center w-full" style="max-width: 360px;">
-      <h2 class="text-4xl font-bold">{current.word}</h2>
-      <p class="italic text-gray-700 my-2">/{current.phonetic}/</p>
-      <div class={'w-full border bg-white rounded p-2 my-6 transition-all ' + (show ? 'opacity-100' : 'opacity-0')}>
-        {#each current.translation as t}
-          <p class="m-1">{t}</p>
-        {/each}
-      </div>
+      <h2 class="text-4xl font-bold" on:click={play} on:keypress={play}>{current.word}</h2>
+      <p class="italic text-gray-700 my-2" on:click={play} on:keypress={play}>/{current.phonetic}/</p>
+      {#if show}
+        <div class="w-full border bg-white rounded p-2 my-6 transition-all">
+          {#each current.translation as t}
+            <p class="m-1">{t}</p>
+          {/each}
+        </div>
+      {:else}
+        <div class="my-6 text-center text-gray-500">说出单词释义<br>查看释义判断正误</div>
+      {/if}
     </div>
     <div class="flex items-center justify-between my-4 font-bold text-lg w-full" style="max-width: 400px;">
       {#if show}
-        <button on:click={() => response(false)} class="transition-all shadow hover:shadow-md rounded w-1/2 p-2 bg-red-500 text-white mx-2">不认识</button>
-        <button on:click={() => response(true)} class="transition-all shadow hover:shadow-md rounded w-1/2 p-2 bg-green-500 text-white mx-2">认识</button>
+        <button on:click={() => response(false)} class="transition-all shadow hover:shadow-md rounded w-1/2 p-2 bg-red-500 text-white mx-2">错误</button>
+        <button on:click={() => response(true)} class="transition-all shadow hover:shadow-md rounded w-1/2 p-2 bg-green-500 text-white mx-2">正确</button>
       {:else}
         <button on:click={() => show = true} class="transition-all shadow hover:shadow-md rounded w-full p-2 bg-blue-500 text-white">查看释义</button>
       {/if}
