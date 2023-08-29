29082982f0ce571413bf1f9a9c9cfcb83ec4cc21
Layzzi <33226552+GrilledSaury@users.noreply.github.com>
2023-08-29 21:50:41 +1000

    finish icons

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,29 @@ import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [sveltekit(), SvelteKitPWA()]
+  plugins: [
+    sveltekit(),
+    SvelteKitPWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Acewords',
+        short_name: 'Acewords',
+        theme_color: '#4980e9',
+        icons: [{
+          src: 'android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        }, {
+          src: 'android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        }, {
+          src: 'android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        }]
+      }
+    })
+  ]
 })
